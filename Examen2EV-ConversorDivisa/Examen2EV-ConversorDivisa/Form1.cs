51790942c4df51c1675e8236d6911d1228c6c750
CNDV2021 <dielma21350@iesmarenostrum.com>
2021-03-16 20:03:13 +0100

    cambios realizados

--- Examen2EV-ConversorDivisa/Examen2EV-ConversorDivisa/Form1.cs
+++ Examen2EV-ConversorDivisa/Examen2EV-ConversorDivisa/Form1.cs
@@ -18,17 +18,17 @@ namespace Ejercicio10
 
          // Declaramos la constante que convierte entre euros y dolares.
         //
-        const double CONV1 = 0.826;  // 1 dolar, 0,826 euros
+        const double CNDV2021 = 0.826;  // 1 dolar, 0,826 euros
         const double CONV2 = 1.1485; // 1 libra, 1,1485 euros
 
         private void btDolaresAEuros_Click(object sender, EventArgs e)
         {
-            double euros, dolares;
+            double euros, dolares, libras;
 
             dolares = double.Parse(tImporte.Text);
-            euros = dolares * CONV1;
+            dolares = dolares* 0.826;
 
-            tResultado.Text = euros.ToString();
+            tResultado.Text = dolares.ToString();
         }
 
         private void btEurosADolares_Click(object sender, EventArgs e)
@@ -36,7 +36,7 @@ namespace Ejercicio10
             double euros, dolares;
 
             euros = double.Parse(tImporte.Text);
-            dolares = euros * CONV1;
+            dolares = euros * 0.826;
 
             tResultado.Text = dolares.ToString();
         }
@@ -46,7 +46,7 @@ namespace Ejercicio10
             double euros, libras;
 
             libras = double.Parse(tImporte.Text);
-            euros = libras * CONV1;
+            euros = libras * 1.1485;
 
             tResultado.Text = euros.ToString();
         }
@@ -56,7 +56,7 @@ namespace Ejercicio10
             double euros, libras;
 
             euros = double.Parse(tResultado.Text);
-            libras = euros / CONV1;
+            libras = euros * 1.1485;
 
             tImporte.Text = libras.ToString();
         }
